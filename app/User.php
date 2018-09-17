d27016f5319d9d9b4c09cf583469977bbde585f0
Ismat Babirli <ismat.babirli@knexel.com>
2018-09-17 10:20:25 +0400

    created Question Model and Migration with Laravel model generator

--- app/User.php
+++ app/User.php
@@ -2,9 +2,8 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -27,4 +26,15 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function questions()
+    {
+        return $this->hasMany(Questions::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes["title"] = $value;
+        $this->attributes["slug"] = str_slug($value);
+    }
 }
